09ad9f2e764e8fcf4485a9fef882e063f359b671
penguine <penguine@localhost.localdomain>
2020-08-13 16:24:19 +0530

    footbal timeline

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+
 import 'package:flutter/material.dart';
+
+import 'timeline/football_timeline.dart';
 
 void main() {
   runApp(MyApp());
@@ -29,39 +32,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ShowcaseFootballTimeline()
     );
   }
 }
